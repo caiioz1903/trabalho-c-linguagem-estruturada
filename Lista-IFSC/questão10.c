1b7c18af214987eba530eec1aade152c7fa122e2
Caio Vitor <161176825+caiioz1903@users.noreply.github.com>
2025-03-25 14:18:11 -0300

    Update questão10.c

--- Lista-IFSC/questão10.c
+++ Lista-IFSC/questão10.c
@@ -4,19 +4,19 @@ int main() {
     // Declaração das variáveis
     int num1, num2, soma;
 
-    // a) Ler o primeiro número inteiro
+    // Ler o primeiro número inteiro
     printf("Digite o primeiro numero inteiro: ");
     scanf("%d", &num1);
 
-    // b) Ler o segundo número inteiro
+    // Ler o segundo número inteiro
     printf("Digite o segundo numero inteiro: ");
     scanf("%d", &num2);
 
-    // c) Efetuar a adição
+    //Efetuar a adição
     soma = num1 + num2;
 
-    // d) Apresentar o valor calculado
+    //Apresentar o valor calculado
     printf("A soma dos numeros é: %d\n", soma);
 
-    return 0;  // Encerra o programa
+    return 0;  //Encerra o programa
 }
