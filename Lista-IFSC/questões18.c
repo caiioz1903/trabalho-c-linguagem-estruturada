fe11f5b793bb7da7cc26481a3ca4debcc7197ed5
Caio Vitor <161176825+caiioz1903@users.noreply.github.com>
2025-03-31 19:52:21 -0300

    Update questões18.c
    comentei o código

--- Lista-IFSC/questões18.c
+++ Lista-IFSC/questões18.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 void main() {
-  int a, quadrado, cubo;
+  int a, quadrado, cubo; // declaração de variáveis
 
-  printf("Digite um número inteiro: ");
-  scanf("%d", &a);
+  printf("Digite um número inteiro: "); 
+  scanf("%d", &a); // armazena o valor
   
-  quadrado = a * a;
-  cubo = a * a * a;
+  quadrado = a * a; // calcula o quadrado
+  cubo = a * a * a; // calcula o cubo
 
-  printf("O quadrado de %d é %d e o cubo é %d", a, quadrado, cubo);
+  printf("O quadrado de %d é %d e o cubo é %d", a, quadrado, cubo); // mostra o resultado
 
   return;
 }
