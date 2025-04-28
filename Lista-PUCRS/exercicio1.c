901a8f88c186eb9a4ac77b51396947756a1c1c23
Caio Vitor <161176825+caiioz1903@users.noreply.github.com>
2025-04-28 19:13:46 -0300

    Update exercicio1.c

--- Lista-PUCRS/exercicio1.c
+++ Lista-PUCRS/exercicio1.c
@@ -1,1 +1,18 @@
+#include <stdio.h> //biblioteca
 
+int main() {
+    int a, i, cont; //variaveis
+    cont = 0;
+
+    for (i = 0; i < 5; i++) { //loop
+        printf("Digite um valor: ");
+        scanf("%d", &a);
+        if (a < 0) { //Contar os negativos
+            cont++;
+        }
+    }
+
+    printf("Quantidade de negativos: %d\n", cont); //imprimir
+
+    return 0;
+}
